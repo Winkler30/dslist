6ee3419771454f870b95c56caf4c29851ee54637
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-07-17 20:42:53 -0300

    Deploy ready

--- src/main/java/com/devchales/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devchales/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devchales.dslist.dto;
 
 import com.devchales.dslist.entities.Game;
+import com.devchales.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	private Long id;
@@ -19,6 +20,15 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		super();
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
--- src/main/java/com/devchales/dslist/services/GameService.java
+++ src/main/java/com/devchales/dslist/services/GameService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devchales.dslist.dto.GameDTO;
 import com.devchales.dslist.dto.GameMinDTO;
 import com.devchales.dslist.entities.Game;
+import com.devchales.dslist.projections.GameMinProjection;
 import com.devchales.dslist.repositories.GameRepository;
 
 @Service
@@ -15,12 +17,20 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
 		Game result = gameRepository.findById(id).get();
 		return new GameDTO(result);
 	}
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
